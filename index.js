b98e1bbc200e7be65c9de04a959add3ec14c7707
doquangvnla <138232649+doquangvnla@users.noreply.github.com>
2024-05-15 15:27:25 +0700

    3
    3

--- index.js
+++ index.js
@@ -11,8 +11,13 @@ app.use((req, res, next) => {
 });
 
 // Route để xử lý yêu cầu proxy
-app.get('/proxy/*', (req, res) => {
-  const url = req.url.replace('/proxy/', 'https://pixels-server.pixels.xyz/v1/marketplace/item/itm_wood?pid=6625e78954c3ca9674476554&v=');
+app.get(['/proxy/*', '/proxy2/*'], (req, res) => {
+  let url;
+  if (req.url.startsWith('/proxy/')) {
+    url = 'https://pixels-server.pixels.xyz/v1/marketplace/item/itm_wood?pid=6625e78954c3ca9674476554&v=';
+  } else if (req.url.startsWith('/proxy2/')) {
+    url = 'https://pixels-server.pixels.xyz/cache/marketplace/listings/count?v=';
+  }
   console.log(`Proxying request to: ${url}`); // Log URL để gỡ lỗi
   request({ url, headers: { 'User-Agent': 'request' } })
     .on('response', (response) => {
@@ -24,6 +29,7 @@ app.get('/proxy/*', (req, res) => {
     })
     .pipe(res);
 });
+
 
 
 // Lắng nghe trên cổng do Vercel cung cấp
